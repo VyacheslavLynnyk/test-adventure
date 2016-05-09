4f5cae71fa48c890f4b697c8467a4b5e9f87a311
VyacheslavLynnyk <vyacheslav.lynnyk@gmail.com>
2016-05-09 21:24:09 +0300

    Manager, test name adds norm

--- controllers/manager.controller.php
+++ controllers/manager.controller.php
@@ -15,20 +15,58 @@ class ManagerController extends Controller
         $this->data['languages'] = Languages::find('all');
         $this->data['tests'] = Tests::find('all');
 
-//        Router::redirect('admin/manager/save-test');
-//        exit();
-        if (isset($_POST['save_tests']) && $_POST['save_tests'] =='save_tests') {
-            if (($_POST['set_language'] == null && $_POST['add_language'] == null) || $_POST['test_name'] == null) {
+        // Save from POST to DB Language and Test (test name)
+        if (isset($_POST['save_test']) && $_POST['save_test'] == 'save') {
+            if (($_POST['set_language'] != null || $_POST['add_language'] != null) && $_POST['test_name'] != null) {
+
+                // TEST var_dump
+                $this->data['var_dump'] = ((bool) $_POST['set_language'] != null) .'('.$_POST['set_language'] . ')|';
+                $this->data['var_dump'] .= ((bool) $_POST['add_language'] != null) .'('.$_POST['add_language'] . ')|' ;
+                $this->data['var_dump'] .= ((bool) $_POST['test_name'] != null).'('.$_POST['test_name'] . ')<br>';
+
+                if ($_POST['add_language'] != null) {
+                    // Save into Languages
+                    $languageModel = new Languages();
+                    $languageModel->language = htmlspecialchars_decode(trim($_POST['add_language']));
+
+                    //TEST var_dump
+                    $this->data['var_dump'] .= '$languageModel->language = '. $languageModel->language . '<br>';
+
+                    if ($languageModel->save() == false) {
+                        Session::setFlash('Ошибка при создании теста!');
+                    }
+                }
+                // Save into Tests
+                $testModel = new Tests();
+                $testModel->test = htmlspecialchars_decode(trim($_POST['test_name']));
+
+                //TEST var_dump
+                $this->data['var_dump'] .= '$testModel->test = '. $testModel->test . '<br>';
+
+                $testModel->language_id = isset($languageModel->id) ? $languageModel->id :Languages::find('last')->id;
+
+                //TEST var_dump
+                $this->data['var_dump'] .= '$testModel->language_id = '. $testModel->language_id . '<br>';
+
+                if ($testModel->save() == false) {
+                    Session::setFlash('Ошибка при создании теста!');
+                } else {
+                    Session::setFlash('Название теста сохранено успешно!');
+
+                    $this->data['languages'] = Languages::find('all');
+                    $this->data['tests'] = Tests::find('all');
+                }
+
+            } else {
                 Session::setFlash('Fill up all needed fields');
                 Router::redirect('admin/manager/index');
-            } else {
-                Session::setFlash('Test name has been saved');
-
-                
             }
-            print_r($_POST);
-        } elseif (isset($_POST['save_tests']) && $_POST['save_tests'] =='save_question') {
 
+        } elseif (isset($_POST['save_test']) && $_POST['save_test'] =='save_question') {
+
+        } else {
+            Session::setFlash('Fill up all needed fields');
+            Router::redirect('admin/manager/index');
         }
     }
 }
--- webroot/js/main-js.js
+++ webroot/js/main-js.js
@@ -3,22 +3,29 @@ $(function() {
 
     /* Answer add */
     $( "#add-answer" ).click(function() {
-        var newInput ='';
+        var newInput = '';
         newInput += '<div class="form-group row">'
-        newInput += '<div class="col-sm-2">';
+        newInput += '<div class="col-sm-1">';
         newInput += '<input type="checkbox" name="answer[]">';
         newInput += '</div>';
-        newInput += '<div class="col-sm-10">';
+        newInput += '<div class="col-sm-11">';
         newInput += '<textarea  class="form-control" name="question"></textarea>';
         newInput +=  '</div>';
         newInput += '</div>';
 
         $( "#answer-list" ).append(newInput);
+
+        $("#remove-answer").show();
+
     });
 
-    $( "#remove-answer").click(function() {
-        if ($("#answer-list div.form-group").length > 1) {
+    $( "#remove-answer" ).click(function() {
+        var answerList = $("#answer-list div.form-group");
+        if (answerList.length > 1) {
             $("#answer-list div.form-group:last-child").remove();
+        }
+        if (answerList.length <= 2) {
+            $("#remove-answer").hide();
         }
     });
 
